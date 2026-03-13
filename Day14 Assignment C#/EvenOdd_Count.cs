880701594e367b0ea680db83f09876aaa367cbb1
Byrisettyswathi <138867768+Byrisettyswathi@users.noreply.github.com>
2026-03-13 08:22:19 +0530

    Add files via upload

---
+++ Day14 Assignment C#/EvenOdd_Count.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1
+{
+    internal class EvenOdd_Count
+    {
+        static void Main(string[] args)
+        {
+            int Even = 0, Odd = 0;
+            Console.WriteLine("Enter the Start numbers: ");
+            int Start = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the End numbers");
+            int End = Convert.ToInt32(Console.ReadLine());
+            for (int i = Start; i <= End; i++) {
+                if (i % 2 == 0)
+                {
+                    Even++;
+    
+            }
+                else
+                {
+                    Odd++;
+                }
+
+            }
+            Console.WriteLine("Even numbers count: " + Even);
+            Console.WriteLine("Odd numbers count: " + Odd);
+        }
+    } }
