dd98dfaa32aca75f8702a1b5e9d0484e5420c388
Swathi <byrisettyswathi3@gmail.com>
2026-03-31 16:09:31 +0530

    Changes Updated

---
+++ Basic Programs C#/Series-Square.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1
+{
+    internal class Series_Square
+    {
+        static void Main(string[] args)
+        {
+            for (int i =0; i<=25; i++)
+            {
+                Console.Write((i*i) +" ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic Programs C#/WelcomeProgram.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment1
+{
+    internal class WelcomeProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the world of c#");
+        }
+    }
+}
