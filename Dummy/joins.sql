e612dd9be1a7eb1f66e24f8a6fa073d63a74f3df
Byrisettyswathi <138867768+Byrisettyswathi@users.noreply.github.com>
2026-04-27 18:07:05 +0530

    Delete Dummy directory

--- Dummy/joins.sql
+++
@@ -1,7 +0,0 @@
-use practice
-select datediff(yy,'2002-05-03', getdate()) as age;
-select datediff(mm,'2002-05-03', getdate()) as age_in_months;
-select datename(weekday,'2002-05-03') as week_name;
-
-
-select datename(weekday,'2002-05-20') as week_name;
